4acbd7e20d89977225ddd8ba1672908d72da859c
Vincent Cui <github@fleeto.us>
2019-01-15 13:35:47 +0800

    limit set to 5

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-set -xe
+set -x
 CODE=`uuidgen`
 TMP="/tmp/$CODE"
 mkdir -p "$TMP"
 cp -Rf * "$TMP"
 cd "$TMP"
 rm -Rf .git
-rm -Rf issueflow/__*
+
 pip install -r requirements.txt  --target .
+find . -name __pycache__ -exec rm -Rf {} \;
+find . -name *.pyc -exec rm -Rf {} \;
+rm build.sh
+
 zip -r9 "/tmp/$CODE.zip" .
 rm -Rf "$TMP"
